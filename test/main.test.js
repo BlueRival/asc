e14103abd09fb4e2d41ecc45f25599092062f0b6
Anthony Hildoer <anthony@bluerival.com>
2024-07-30 10:19:15 -0600

    release 2.0.4

--- test/main.test.js
+++ test/main.test.js
@@ -1,5 +1,4 @@
 'use strict';
 
-require( './suites/eslint.test' );
-require( './suites/general.test' );
-require( './suites/caching.test' );
+require('./suites/general.test');
+require('./suites/caching.test');
